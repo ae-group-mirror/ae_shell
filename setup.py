fa7c1f8602e2ade36614e4c697773d0334bf49b9
AndiEcker <aecker2@gmail.com>
2025-10-12 13:49:13 +0100

    v0.3.7: enhanced the robustness of mask_token() for malformed text blocks
    M  README.md
    M  ae/shell.py
    M  setup.py
    M  tests/test_shell.py

--- setup.py
+++ setup.py
@@ -25,13 +25,13 @@ setup_kwargs = {
     'license': 'GPL-3.0-or-later',
     'long_description': ('<!-- THIS FILE IS EXCLUSIVELY MAINTAINED by the project ae.ae v0.3.101 -->\n'
  '<!-- THIS FILE IS EXCLUSIVELY MAINTAINED by the project aedev.namespace_root_tpls v0.3.22 -->\n'
- '# shell 0.3.6\n'
+ '# shell 0.3.7\n'
  '\n'
  '[![GitLab develop](https://img.shields.io/gitlab/pipeline/ae-group/ae_shell/develop?logo=python)](\n'
  '    https://gitlab.com/ae-group/ae_shell)\n'
  '[![LatestPyPIrelease](\n'
- '    https://img.shields.io/gitlab/pipeline/ae-group/ae_shell/release0.3.6?logo=python)](\n'
- '    https://gitlab.com/ae-group/ae_shell/-/tree/release0.3.6)\n'
+ '    https://img.shields.io/gitlab/pipeline/ae-group/ae_shell/release0.3.7?logo=python)](\n'
+ '    https://gitlab.com/ae-group/ae_shell/-/tree/release0.3.7)\n'
  '[![PyPIVersions](https://img.shields.io/pypi/v/ae_shell)](\n'
  '    https://pypi.org/project/ae-shell/#history)\n'
  '\n'
@@ -108,7 +108,7 @@ setup_kwargs = {
         'Source': 'https://ae.readthedocs.io/en/latest/_modules/ae/shell.html'},
     'python_requires': '>=3.9',
     'url': 'https://gitlab.com/ae-group/ae_shell',
-    'version': '0.3.6',
+    'version': '0.3.7',
     'zip_safe': True,
 }
 
