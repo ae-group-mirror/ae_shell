685d943fd2c944a352c9a75b221d06bdffb87667
AndiEcker <aecker2@gmail.com>
2025-11-19 15:38:19 +0000

    v0.3.8: migrated temp_context* helpers to ae.core, exit_error() to App.shutdown(), check_if() to App.chk(), and removed get_main_app() and MockedMainApp
    M  .gitignore
    M  .gitlab-ci.yml
    M  CONTRIBUTING.rst
    M  LICENSE.md
    M  README.md
    M  SECURITY.md
    M  ae/shell.py
    M  dev_requirements.txt
    M  pyproject.toml
    M  setup.py
    M  tests/conftest.py
    M  tests/requirements.txt
    M  tests/test_shell.py

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-# THIS FILE IS EXCLUSIVELY MAINTAINED by the project aedev.project_tpls v0.3.58
+# THIS FILE IS EXCLUSIVELY MAINTAINED by the project aedev.project_tpls v0.3.68
 """ setup of ae namespace module portion shell: shell execution and environment helpers. """
 # noinspection PyUnresolvedReferences
 import sys
@@ -12,7 +12,7 @@ setup_kwargs = {
     'author_email': 'aecker2@gmail.com',
     'classifiers': [       'Development Status :: 3 - Alpha', 'Natural Language :: English', 'Operating System :: OS Independent',
         'Programming Language :: Python', 'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.9', 'Topic :: Software Development :: Libraries :: Python Modules',
+        'Programming Language :: Python :: 3.12', 'Topic :: Software Development :: Libraries :: Python Modules',
         'Typing :: Typed'],
     'description': 'ae namespace module portion shell: shell execution and environment helpers',
     'extras_require': {       'dev': [       'aedev_project_tpls', 'ae_ae', 'anybadge', 'coverage-badge', 'aedev_project_manager', 'flake8',
@@ -25,13 +25,13 @@ setup_kwargs = {
     'license': 'GPL-3.0-or-later',
     'long_description': ('<!-- THIS FILE IS EXCLUSIVELY MAINTAINED by the project ae.ae v0.3.101 -->\n'
  '<!-- THIS FILE IS EXCLUSIVELY MAINTAINED by the project aedev.namespace_root_tpls v0.3.22 -->\n'
- '# shell 0.3.7\n'
+ '# shell 0.3.8\n'
  '\n'
  '[![GitLab develop](https://img.shields.io/gitlab/pipeline/ae-group/ae_shell/develop?logo=python)](\n'
  '    https://gitlab.com/ae-group/ae_shell)\n'
  '[![LatestPyPIrelease](\n'
- '    https://img.shields.io/gitlab/pipeline/ae-group/ae_shell/release0.3.7?logo=python)](\n'
- '    https://gitlab.com/ae-group/ae_shell/-/tree/release0.3.7)\n'
+ '    https://img.shields.io/gitlab/pipeline/ae-group/ae_shell/release0.3.8?logo=python)](\n'
+ '    https://gitlab.com/ae-group/ae_shell/-/tree/release0.3.8)\n'
  '[![PyPIVersions](https://img.shields.io/pypi/v/ae_shell)](\n'
  '    https://pypi.org/project/ae-shell/#history)\n'
  '\n'
@@ -108,7 +108,7 @@ setup_kwargs = {
         'Source': 'https://ae.readthedocs.io/en/latest/_modules/ae/shell.html'},
     'python_requires': '>=3.9',
     'url': 'https://gitlab.com/ae-group/ae_shell',
-    'version': '0.3.7',
+    'version': '0.3.8',
     'zip_safe': True,
 }
 
